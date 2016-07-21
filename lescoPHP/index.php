103c1ce447fbe97ec424428db48aff3b65262471
hsolicha <hsolicha@hotmail.com>
2016-07-21 12:44:07 -0600

    Eliminación de imagenes, cambio de métodos.

--- lescoPHP/index.php
+++ lescoPHP/index.php
@@ -4,102 +4,133 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
+<html lang="en">
     <head>
-        <meta charset="UTF-8">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Lesco-Text</title>
+        
+         <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
     </head>
     <body>
-        <h3>Lesco a Texto</h3>
-        <form enctype="multipart/form-data" action="index.php" method="POST">
-            <label for="imagen">Imagen: </label>
-            <input type="file" name="imagen" id="imagen" accept="image/png">
-            <input type="submit" name="subir" value="Traducir" />
-        </form>
+        <div class="navbar-wrapper">
+            <div class="container-fluid">
+
+                <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">Lesco-Texto</a>
+                        </div>
+                        <div id="navbar" class="navbar-collapse collapse">
+                            <ul class="nav navbar-nav">
+                                <li><a href="diccionario.html">Diccionario</a></li>
+                                <li class="active"><a href="#traduccion">Traducción</a></li>
+                                <li><a href="#contactar">Sobre</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+            </div>
+        </div>
+        
+        <div id="chapter">
+        <!-- FIN Navbar-->
         <?php
-            include 'comparer.php';
+        include 'comparer.php';
 
-             echo ('Hola Esteban esto es una prueba para probar lo que estamos probando.');
-            $comparador = new comparadorImg(8);
-            
-            $imgArray = [
-                "a" =>  "a.png",
-                "b" =>  "b.png",
-                "c" =>  "mina.jpg"
-            ];
-            
-            
-            //comprobamos si ha ocurrido un error.
-            if ($_FILES["imagen"]["error"] > 0) {
-                echo "ha ocurrido un error";
+        $comparador = new comparadorImg(8);
+        $palabra = "";
+
+        $imgArray = [
+            //"h" =>  "Henry.png",
+            //"e" =>  "Esteban.png",
+            "a" =>  "Carlos.png",
+            "o" =>  "O-Carlos.png",
+            "o1" =>  "O-Henry.png",
+            "o2" =>  "O-Esteban.png",
+            "h" =>  "a3WEB.png"
+            /*"o" =>  "a1.png",
+            "o1" =>  "a1W.png",
+            "o2" =>  "a1WEB.png",
+            "a2" =>  "a2.png",
+            "a3" =>  "a2WEB.png",
+            "h" =>  "a3.png",
+            "h1" =>  "a3WEB.png",
+            "o3" =>  "a4.png",
+            "h1" =>  "h.png",
+            "o4" =>  "o.png"*/
+        ];
+        
+        function blackWhite($image1){
+            $f1 = $image1;
+
+            $im = imagecreatefrompng($f1);
+
+            if ($im && imagefilter($im, IMG_FILTER_GRAYSCALE)) {
+                echo 'Imagen convertida a escala de grises.' . "<br><br>";
+
+                imagepng($im, $f1); //convierte la imagen y la asigna a la imagen enviada
             } else {
-                //ahora vamos a verificar si el tipo de archivo es un tipo de imagen permitido.
-                //y que el tamano del archivo no exceda los 100kb
-                $permitidos = "image/png";
-                $limite_kb = 10000;
-
-                if (in_array($_FILES['imagen']['type'], $permitidos) && $_FILES['imagen']['size'] <= $limite_kb * 1024) {
-                    //esta es la ruta donde copiaremos la imagen
-                    //recuerden que deben crear un directorio con este mismo nombre
-                    //en el mismo lugar donde se encuentra el archivo subir.php
-                    $ruta = "img/" . $_FILES['imagen']['name'];
-                    //comprobamos si este archivo existe para no volverlo a copiar.
-                    //pero si quieren pueden obviar esto si no es necesario.
-                    //o pueden darle otro nombre para que no sobreescriba el actual.
-                    if (!file_exists($ruta)) {
-                        //aqui movemos el archivo desde la ruta temporal a nuestra ruta
-                        //usamos la variable $resultado para almacenar el resultado del proceso de mover el archivo
-                        //almacenara true o false
-                        $resultado = @move_uploaded_file($_FILES["imagen"]["tmp_name"], $ruta);
-                        if ($resultado) {
-                            $comparador = new comparadorImg(8);
-                            
-                            foreach($imgArray as &$valor){
-                                $f1 = $_FILES['imagen'];
-                                $f2 = './img/'.$valor;
-
-                                $im = imagecreatefrompng($f1);
-
-                                if($im && imagefilter($im, IMG_FILTER_GRAYSCALE))
-                                {
-                                    echo 'Imagen convertida a escala de grises.'."<br><br>";
-
-                                    imagepng($im, $f1); //convierte la imagen y la asigna a la imagen enviada
-
-
-
-                                }
-                                else
-                                {
-                                    echo 'La conversión a escala de grises falló.';
-                                }
-
-                                imagedestroy($im);
-
-
-
-
-
-                                $hash       = $comparador->getHash_img('./img/mina.jpg');
-                                //echo $hash;
-                                $dif        = $comparador->comparar_imgs($f1,$f2);
-
-
-                                echo "<img width='400' height='300' src='".$f1."'><br><br>";	
-
-                                echo "<b>Diferencias</b> ".$dif."%<br>";
-                                echo "<b>Similitudes</b> ".(100-$dif)."%<br>";
-                            }
-                        } else {
-                            echo "ocurrio un error al mover el archivo.";
-                        }
-                    } else {
-                        echo $_FILES['imagen']['name'] . ", este archivo existe";
-                    }
-                } else {
-                    echo "archivo no permitido, es tipo de archivo prohibido o excede el tamano de $limite_kb Kilobytes";
-                }
+                echo 'La conversión a escala de grises falló.';
             }
-        ?>
+
+            imagedestroy($im);
+        }
+
+        foreach ($imgArray as $i => $valor) {
+            //echo $imgArray[$i] ." - ".$valor." - ".$i.'<br>';
+            $f1 = "./img/O-Henry.png";
+            //$f2 = "./img/a1w.png";
+            $f2 = "./img/".$valor;
+            
+            //blackWhite($f1);
+            //blackWhite($f2);
+          
+            $hash = $comparador->getHash_img($f1);
+            //echo $hash;
+            $dif = $comparador->comparar_imgs($f1, $f2);
+            
+            echo "<div class = 'row'>";
+            echo "<img class = 'col-md-4' width='400' height='300' src='" . $f1 . "'>";
+            echo "<img class = 'col-md-4' width='400' height='300' src='" . $f2 . "'><br><br>";
+            echo "</div>";
+            echo "<b>Diferencias</b> " . $dif . "%<br>";
+            echo "<b>Similitudes</b> " . (100 - $dif) . "%<br>";
+            
+           
+            
+            if((100 - $dif) >= 50){
+                 $palabra .= $i;
+                
+                echo "<b>Letra: </b>".$palabra.'<br>';
+                //break;
+                
+            }
+
+            
+        }
+       
+        
+            ?>
+        </div>
     </body>
-</html>
+<html>
